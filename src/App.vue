5c106a8ad13f04f2b03a2445b48d8f9d3070b0d0
zukio <as.web554@gmail.com>
2024-07-08 13:10:37 +0900

    デプロイテスト２

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
@@ -19,7 +16,6 @@
 
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <style scoped>
@@ -37,7 +33,6 @@ nav {
   width: 100%;
   font-size: 12px;
   text-align: center;
-  margin-top: 2rem;
 }
 
 nav a.router-link-exact-active {
