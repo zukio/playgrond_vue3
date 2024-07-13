92d7f9fd4ec575a75cb4a9ccc12eb4ca605fe241
zukio <as.web554@gmail.com>
2024-07-13 09:54:48 +0900

    baseurl

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/playgrond_vue3/' : '/',
+  base: process.env.BASE_URL || '/',
   plugins: [
     vue({
       template: {
