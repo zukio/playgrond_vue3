c01f3db6911532de19671579cf5400402cf8ff8d
zukio <as.web554@gmail.com>
2024-07-21 17:02:41 +0900

    Update index.ts

--- utils/index.ts
+++ utils/index.ts
@@ -1,6 +1,6 @@
-export const debounce = (func, wait) => {
-  let timeout;
-  return (...args) => {
+export const debounce = (func: (...args: any[]) => void, wait: number): ((...args: any[]) => void) => {
+  let timeout: NodeJS.Timeout;
+  return (...args: any[]): void => {
     clearTimeout(timeout);
     timeout = setTimeout(() => func.apply(this, args), wait);
   };
